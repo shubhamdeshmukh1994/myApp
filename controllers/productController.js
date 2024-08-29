db00c6e85a9f54df55ab14f23ebb33ec685afda5
shubham <shubhamsolanke@falkondata.com>
2024-08-29 16:38:08 +0530

    product validation done

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,14 +1,19 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
+const {
+  validateProductReq,
+  validateUpdateProduct,
+} = require("./validation.services/product.request.validation");
 
 exports.createProduct = async (req, res) => {
-  const {
-    product_name: productName,
-    description,
-    img_url: imgUrl,
-    price,
-  } = req.body;
+  const requestData = req.body;
   const createdBy = req.user.user_uid;
   try {
+    const {
+      product_name: productName,
+      description,
+      img_url: imgUrl,
+      price,
+    } = validateProductReq({ requestData });
     const product_uid = await Product.create({
       productName,
       description,
@@ -20,43 +25,48 @@ exports.createProduct = async (req, res) => {
       .status(201)
       .json({ message: "Product created successfully", product_uid });
   } catch (error) {
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: `Server error : ${error.message}`});
   }
 };
 
 exports.getVisibleProducts = async (req, res) => {
-    try {
-        const products = await Product.findAllVisible();
-        res.json(products);
-    } catch (error) {
-        res.status(500).json({ message: 'Server error' });
-    }
+  try {
+    const products = await Product.findAllVisible();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 exports.updateProduct = async (req, res) => {
-    const { product_uid: productId } = req.params;
-    const updates = req.body;
-    try {
-        const userId = req.user.user_uid;
-        const affectedRows = await Product.update(productId, updates, userId);
-        if (affectedRows === 0) return res.status(404).json({ message: 'Product not found' });
+  const { product_uid: productId } = req.params;
+  const updates = req.body;
+  try {
+    const requestData = req.body;
+    requestData.product_uid = productId;
+    validateUpdateProduct({requestData});
+    const userId = req.user.user_uid;
+    const affectedRows = await Product.update(productId, updates, userId);
+    if (affectedRows === 0)
+      return res.status(404).json({ message: "Product not found" });
 
-        res.json({ message: 'Product updated successfully' });
-    } catch (error) {
-        console.log("error while update product", error);
-        res.status(500).json({ message: 'Server error' });
-    }
+    res.json({ message: "Product updated successfully" });
+  } catch (error) {
+    console.log("error while update product", error);
+    res.status(500).json({ message: `Server error : ${error.message}` });
+  }
 };
 
 exports.deleteProduct = async (req, res) => {
-    try {
-        const { product_uid: productId } = req.params;
-        const affectedRows = await Product.deleteProduct(productId);
-        if (affectedRows === 0) return res.status(404).json({ message: 'Product not found' });
+  try {
+    const { product_uid: productId } = req.params;
+    const affectedRows = await Product.deleteProduct(productId);
+    if (affectedRows === 0)
+      return res.status(404).json({ message: "Product not found" });
 
-        res.json({ message: 'Product deleted successfully' });
-    } catch (error) {
-        console.log("error while deleting product", error);
-        res.status(500).json({ message: 'Server error' });
-    }
+    res.json({ message: "Product deleted successfully" });
+  } catch (error) {
+    console.log("error while deleting product", error);
+    res.status(500).json({ message: `Server error : ${error.message}` });
+  }
 };
---
+++ controllers/validation.services/product.request.validation.js
@@ -0,0 +1,61 @@
+const Joi = require("joi");
+const validateProductReq = function ({ requestData }) {
+  try {
+    const schema = Joi.object({
+      ["product_name"]: Joi.string().required(),
+      ["description"]: Joi.string().allow("", null).optional(),
+      ["img_url"]: Joi.string().required(),
+      ["price"]: Joi.number().precision(2).required(),
+    });
+
+    const { error: { details } = {} } = schema.validate({
+      ["product_name"]: requestData["product_name"],
+      ["description"]: requestData["description"],
+      ['img_url']: requestData["img_url"],
+      ["price"]: requestData["price"],
+    });
+
+    if (details) {
+      throw details[0];
+    }
+    return requestData;
+  } catch (error) {
+    console.error(`While validate Request Body,
+    Error:${error}, Error Json: ${JSON.stringify(error)}`);
+    throw error;
+  }
+};
+
+const validateUpdateProduct = function ({ requestData }) {
+  try {
+    const schema = Joi.object({
+        ["product_uid"]: Joi.string().required(),
+        ["product_name"]: Joi.string().optional(),
+        ["description"]: Joi.string().allow("", null).optional(),
+        ["img_url"]: Joi.string().optional(),
+        ["price"]: Joi.number().precision(2).optional(),
+    });
+
+    const { error: { details } = {} } = schema.validate({
+        ["product_uid"]: requestData["product_name"],
+        ["product_name"]: requestData["product_name"],
+        ["description"]: requestData["description"],
+        ['img_url']: requestData["img_url"],
+        ["price"]: requestData["price"],
+    });
+
+    if (details) {
+      throw details[0];
+    }
+    return requestData;
+  } catch (error) {
+    console.error(`While validate Request Body,
+    Error:${error}, Error Json: ${JSON.stringify(error)}`);
+    throw error;
+  }
+};
+
+module.exports = {
+    validateProductReq,
+    validateUpdateProduct,
+};
