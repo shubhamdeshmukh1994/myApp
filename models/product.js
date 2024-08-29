66eb4439e80937bf7ebb0cc01425fe23ed89b1ed
shubham <shubhamsolanke@falkondata.com>
2024-08-29 18:30:35 +0530

    changes done

--- models/product.js
+++ models/product.js
@@ -28,7 +28,7 @@ class Product {
 
   static async findProduct({ productName, price }) {
     const [rows] = await db.execute(
-      "SELECT * FROM products WHERE product_name LIKE ? AND price = ?;",
+      "SELECT * FROM products WHERE product_name LIKE ? AND price = ? AND isactive = true;",
       [`%${productName}%`, price]
     );
     return rows[0];
